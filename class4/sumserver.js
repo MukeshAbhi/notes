612c1cf6252a37b6d0b98450c297a269df2ad856
MukeshAbhi <abhimukesh284@gmail.com>
2024-09-19 12:59:15 +0530

    interest completed

--- class4/sumserver.js
+++ class4/sumserver.js
@@ -1,5 +1,8 @@
 const express = require( 'express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());// to bypass chrome security 
 
 app.get('/sum', (req,res)=> {
     const a = parseInt(req.query.a);
@@ -8,4 +11,19 @@ app.get('/sum', (req,res)=> {
     res.send(sum.toString());
 })
 
+app.get('/interest',(req,res) => {
+    const { principal, rate, time } = req.query;
+
+    const principalParsed = Number(principal);
+    const rateParsed = Number(rate);
+    const timeParsed = Number(time);
+
+    const interest = principalParsed * (rateParsed/100) * timeParsed;
+    const total = interest + principalParsed;
+    res.json({
+        interest: interest.toString(),
+        total: total.toString()
+    });
+
+})
 app.listen(3000);
