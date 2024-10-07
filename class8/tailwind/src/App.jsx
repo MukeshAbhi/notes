a69ce62a8197235da4d2555426f8b09a81c3dd63
MukeshAbhi <abhimukesh284@gmail.com>
2024-10-07 23:04:13 +0530

    learing tailwind

---
+++ class8/tailwind/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <> 
+      <div className='grid grid-cols-1   md:grid-cols-3'>  
+        <div className='bg-red-500'>fdefweew</div>
+        <div className='bg-green-500'>wffss</div>
+        <div className='bg-blue-500'>ewfew</div>
+      </div>
+    </>
+  )
+}
+
+export default App
